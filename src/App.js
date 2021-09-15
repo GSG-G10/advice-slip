6e170ceef087d72ad52935b2c9b3e5253d323119
muhammadharoun <hro19502001@gmail.com>
2021-09-15 14:57:26 +0300

    fix bable-jest bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import   AdvicesList from '../src/component/AdvicesList'
-import GetAdvise from "./utils/GetAdvise";
+// import GetAdvise from "./utils/GetAdvise";
 class  App extends Component {
+  // eslint-disable-next-line no-useless-constructor
   constructor(){
     super()
   
