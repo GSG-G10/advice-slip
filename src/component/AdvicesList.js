a0258e10473701bb4856ff99b9dace79f9848072
muhammadharoun <hro19502001@gmail.com>
2021-09-16 10:52:33 +0300

    add responsive to page

--- src/component/AdvicesList.js
+++ src/component/AdvicesList.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 class AdvicesList extends Component{
     render(){
+        let data
+        if (this.props.data === undefined) {
+             data = [    {
+                id: 1,
+                advice: "The requested data was not found",
+                category:"",
+                date: ""
+                },]
+        }else{
+             data = this.props.data
+        }
         return (
             <div>
               <h1>Advices List</h1>
-               <ul>
-                   {this.props.data.map(data=>(
+               <ul>     
+                   {data.map(data=>(
                        <div className='card'>
                        <li>{data.advice}</li>
                        <li>{data.category}</li>
